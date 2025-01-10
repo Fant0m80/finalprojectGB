3c6eb84b7e6c01cfdbb3912b5afd8f286c931a89
Fant0m80 <fant.home80@gmail.com>
2025-01-10 12:05:35 +0100

    COm

--- FinalProjectGB/src/main/java/fr/isep/vindev/finalprojectgb/controller/ProjetController.java
+++ FinalProjectGB/src/main/java/fr/isep/vindev/finalprojectgb/controller/ProjetController.java
@@ -1,4 +1,17 @@
 package fr.isep.vindev.finalprojectgb.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+import java.io.IOException;
+
 public class ProjetController {
+
+    @FXML
+    private Button Button_AjouterTache;
+
+    @FXML
+    protected void onButton_AjouterTacheClick() throws IOException{
+        
+    }
 }
