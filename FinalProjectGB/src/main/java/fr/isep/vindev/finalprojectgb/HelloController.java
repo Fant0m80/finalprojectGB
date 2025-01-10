1e83d4ac846bd01e93b9e90f2d8f62254f17b92c
Fant0m80 <fant.home80@gmail.com>
2025-01-10 09:07:07 +0100

    Apps

--- FinalProjectGB/src/main/java/fr/isep/vindev/finalprojectgb/HelloController.java
+++ FinalProjectGB/src/main/java/fr/isep/vindev/finalprojectgb/HelloController.java
@@ -1,14 +1,42 @@
 package fr.isep.vindev.finalprojectgb;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Button Button_Projet;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button Button_Employe;
+
+    @FXML
+    protected void onButton_ProjetClick() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("ProjetApp.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+
+        Stage newstage = new Stage();
+        newstage.setTitle("Projet");
+        newstage.setScene(scene);
+
+        newstage.show();
+    }
+
+    @FXML
+    protected void onButton_EmployeClick() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("EmployeApp.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+
+        Stage newstage = new Stage();
+        newstage.setTitle("Employe");
+        newstage.setScene(scene);
+
+        newstage.show();
     }
 }
