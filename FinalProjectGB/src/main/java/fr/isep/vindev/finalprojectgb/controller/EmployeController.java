1eba71cf5ef0cdd7453bda3f511d6c534725b66e
VINCENT Gabriel <vincentgab78@gmail.com>
2025-01-14 20:58:15 +0100

    affichage des projets d'un employé

--- FinalProjectGB/src/main/java/fr/isep/vindev/finalprojectgb/controller/EmployeController.java
+++ FinalProjectGB/src/main/java/fr/isep/vindev/finalprojectgb/controller/EmployeController.java
@@ -3,7 +3,9 @@ package fr.isep.vindev.finalprojectgb.controller;
 import fr.isep.vindev.finalprojectgb.Employe;
 import fr.isep.vindev.finalprojectgb.Projet;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 
 import java.util.ArrayList;
@@ -25,7 +27,7 @@ public class EmployeController {
     private Label tel;
 
     @FXML
-    private HBox HBox_listeProjet;
+    private GridPane GridPane_listeProjet;
 
     public void afficherEmploye(Employe selectedEmploye) {
         nom.setText(selectedEmploye.getNom());
@@ -38,12 +40,40 @@ public class EmployeController {
     }
 
     private void afficherProjets(ArrayList<Projet> listeProjet) {
-        HBox_listeProjet.getChildren().clear();
-
+        GridPane_listeProjet.getChildren().clear();
+        int c = 0;
         for (Projet projet : listeProjet) {
-            Label projetLabel = new Label(projet.getNomDuProjet());
-            projetLabel.setStyle("-fx-border-color: #d21a1a; -fx-padding: 5;");
-            HBox_listeProjet.getChildren().add(projetLabel);
+            for (int j=0; j<GridPane_listeProjet.getColumnCount(); j++) {
+                for (int i = 0; i < GridPane_listeProjet.getRowCount(); i++) {
+                    if (isCaseEmpty(GridPane_listeProjet, i, j)) {
+                        Label projetLabel = new Label(projet.getNomDuProjet());
+                        projetLabel.setStyle("-fx-border-color: #d21a1a; -fx-padding: 5;");
+                        GridPane_listeProjet.add(projetLabel, i, j);
+                        c+=1;
+                        break;
+                    }
+                }
+                if (c == 1){
+                    c = 0;
+                    break;
+                }
+            }
         }
+    }
+
+
+    private boolean isCaseEmpty(GridPane grid, int row, int col) {
+        for (Node node : grid.getChildren()) {
+            Integer rowIndex = GridPane.getRowIndex(node);
+            Integer colIndex = GridPane.getColumnIndex(node);
+
+            rowIndex = (rowIndex == null) ? 0 : rowIndex;
+            colIndex = (colIndex == null) ? 0 : colIndex;
+
+            if (rowIndex == row && colIndex == col) {
+                return false;
+            }
+        }
+        return true;
     }
 }
